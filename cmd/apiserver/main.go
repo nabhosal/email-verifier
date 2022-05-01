6357edfc82c8519d9e15f3925986447e24b39bd7
nabhosal <nileshxbhosale@gmail.com>
2022-05-01 21:37:13 +0530

    Enable SMTP check

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	verifier := emailVerifier.NewVerifier()
+	verifier := emailVerifier.NewVerifier().EnableSMTPCheck()
 	ret, err := verifier.Verify(ps.ByName("email"))
 	if err != nil {
 		http.Error(w, err.Error(), 500)
